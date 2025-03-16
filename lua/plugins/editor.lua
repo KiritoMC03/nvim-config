6e7aff8682bee12c9efe7aa0fb55394d33869c90
Kirill Dudnik <kirill.dulim@gmail.com>
2025-03-16 16:12:39 +0100

    lspconfig, neo-tree

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,18 @@
+return {
+    -- add folding range to capabilities
+    {
+        "neovim/nvim-lspconfig",
+    },
+
+    -- file explorer
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+          "MunifTanjim/nui.nvim",
+          -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    },
+}
