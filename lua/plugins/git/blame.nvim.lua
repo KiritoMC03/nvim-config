94054ae1071ada18d8bd21426e55cc2a89582204
Kirill Dudnik <kirill.dulim@gmail.com>
2025-03-22 18:11:52 +0100

    added blame to view git diff

---
+++ lua/plugins/git/blame.nvim.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "FabijanZulj/blame.nvim",
+        lazy = false,
+        config = true,
+        keys = {
+            {
+                "<leader>gv",
+                ":BlameToggle window<CR>",
+                desc = "Git visualize dif",
+            },
+        },
+    },
+}
