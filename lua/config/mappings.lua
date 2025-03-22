af26bdbeee60cbea53981bb929df1d250fa6a91b
Kirill Dudnik <kirill.dulim@gmail.com>
2025-03-22 21:52:59 +0100

    relative-absolute lines switching

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -3,3 +3,9 @@ vim.keymap.set('n', '<leader>nf', ':Neotree left focus<CR>')
 vim.keymap.set('n', '<leader>nt', ':Neotree left toggle<CR>')
 vim.keymap.set('n', '<leader>q', ':q<CR>')
 
+-- Custom
+local switch_lines = function ()
+    vim.opt.relativenumber = not vim.opt.relativenumber:get()
+    vim.opt.number = not vim.opt.number:get()
+end
+vim.keymap.set('n', '<leader>dl', switch_lines)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,6 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.autoindent = true
+vim.opt.rnu = true
 
 vim.g.mapleader = " "
