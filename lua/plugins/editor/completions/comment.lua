30f6cccdd6f07e52329a9d28f8aec8060d8fafcb
Kirill Dudnik <kirill.dulim@gmail.com>
2025-03-23 15:42:01 +0100

    comment

---
+++ lua/plugins/editor/completions/comment.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "numToStr/Comment.nvim",
+        enabled = true,
+        opts = {
+            toggler = {
+                line = "<leader>/",
+            },
+            opleader = {
+                line = "<leader>/"
+            },
+        }
+    }
+}
